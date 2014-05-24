43c751d430fecd25ba8cd5f6da335fe7f68a5a23
Nick Simmons <nick.simmons@jadedpixel.com>
2014-05-24 16:44:25 -0400

    [chef] open default Rails port in iptables

--- chef/my_cookbooks/sandbox/recipes/default.rb
+++ chef/my_cookbooks/sandbox/recipes/default.rb
@@ -1,3 +1,11 @@
 file '/home/vagrant/install.sh' do
   action :delete
 end
+
+bash "open incoming port 3000" do
+  user "root"
+  code <<-EOH
+  iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
+  service iptables save
+  EOH
+end
